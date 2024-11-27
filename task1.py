797962b6131d39bae3cd4e544136b116c89c8354
Kronkan <h22thsta@du.se>
2024-11-27 09:50:35 +0100

    additional tasks

--- task1.py
+++ task1.py
@@ -1,1 +1,11 @@
-
+"""    
+    1. Create at least 10 cars/vehicles as agents.
+    2. Create at least 5 parking spaces as agents.
+    3. Create a parking lot model using multigrid.
+    4. At each step the car should move and find the parking space. You can decide how the car moves to reach the parking space.
+    5. Let the car leave the parking space after 3 to 5 steps.
+    6. Store the data on steps using data collector and show how many steps it took the cars to occupy a parking space.
+    7. Display this simulation graphically.
+    8. Increase or decrease parking spaces and cars and see how it simulates. Provide your reflection on this.
+    9. (Optional) Create an obstacle in the parking lot which can represent trees, use A* algorithm to move the car towards the parking space.
+"""
---
+++ task3.py
@@ -0,0 +1,7 @@
+"""
+    1. Use the textfile named “pg2701”
+    2. Split the text file to chunks (group of sentences)
+    3. Clean the data and count the words using single and multi-thread
+    4. Identify processing times between them
+    5. Explain the difference in performance
+"""
